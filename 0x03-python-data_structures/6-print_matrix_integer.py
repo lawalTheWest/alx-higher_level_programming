2f8b8bee0136461aec74094abb95790d88f2bd56
Lawal Tajudeen Ogunsola <lwltjdn@gmail.com>
2023-08-14 23:58:25 +0100

    a function that prints a matrix of integers.wq

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''a function that prints a matrix of integers'''
+
+
+def print_matrix_integer(matrix=[[]]):
+    """Prints a matrix of ints"""
+    for row in matrix:
+        for i in row:
+            if i != row[-1]:
+                print('{:d} '.format(i), end="")
+            else:
+                print('{:d}'.format(i), end="")
+        print()
