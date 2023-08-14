ada96fb01da30e2863a171574a6d837a5964cfbf
Lawal Tajudeen Ogunsola <lwltjdn@gmail.com>
2023-08-14 16:58:06 +0100

    a program that imports all functions from the file calculator_1.py and handles basic operations.00

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,18 +2,20 @@
 
 if __name__ == "__main__":
     """Handle basic arithmetic operations."""
-    from calculator_1 import add, sub, mul, div
     import sys
+    # def arithmetic():
+    from calculator_1 import add, sub, mul, div
 
     if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-        ops = {'+': add, '-': sub, '*': mul, '/': div}
-        if sys.argv[2] not in list(ops.keys()):
-            print("Unknown operator. Available operators: +, -, * and /")
-            sys.exit(1)
-            a = int(sys.argv[1])
-            b = int(sys.argv[3])
-            op = sys.argv[2]
-            print('{} {} {} = {}'.format(a, op, b, ops[op](a, b)))
+    ops = {'+': add, '-': sub, '*': mul, '/': div}
+    if sys.argv[2] not in list(ops.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    op = sys.argv[2]
+    print('{} {} {} = {}'.format(a, op, b, ops[op](a, b)))
