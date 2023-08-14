165307dc0c839686ffaaa4f8a0108e6b29cbb4dd
Lawal Tajudeen Ogunsola <lwltjdn@gmail.com>
2023-08-15 00:57:54 +0100

    a function that adds 2 tuples.

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,7 +2,7 @@
 
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    return tuple(sum(t) for t in zip(
+    return tuple(sum(i) for i in zip(
         (tuple_a + (0,) * ((not tuple_a) + 1))[:2],
         (tuple_b + (0,) * ((not tuple_b) + 1))[:2],
     ))
