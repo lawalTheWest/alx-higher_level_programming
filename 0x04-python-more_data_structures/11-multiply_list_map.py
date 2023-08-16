160d4a45cefd244c964b3e02e31f5c4c229ebf66
lawal tajudeen ogunsola <lwltjdn@gmail.com>
2023-08-16 15:10:12 +0100

    updated the list

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -8,5 +8,5 @@
 
 def multiply_list_map(my_list=[], number=0):
     product = map((lambda i: i * number), my_list)
-    product = list(product)
-    return (product)
+    product_list = list(product)
+    return (product_list)
