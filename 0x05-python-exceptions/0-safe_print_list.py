f22b81224bc6b3eddca0c3289767cecf5ccb8542
lawal tajudeen ogunsola <lwltjdn@gmail.com>
2023-08-29 12:02:54 +0100

    Updated a function that prints x elements of a list by adding an exception type - IndexError

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,14 +4,13 @@
 '''
 
 
-
 def safe_print_list(my_list=[], x=0):
     elements_printed = 0
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
             elements_printed += 1
-        except:
+        except IndexError:
             break
     print("")
     return (elements_printed)
