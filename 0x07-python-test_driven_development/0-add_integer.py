972f97ae091b17dfb8f1b9ddb1d4f79d2cc5297d
lawal tajudeen ogunsola <lwltjdn@gmail.com>
2023-09-07 21:09:51 +0100

    a function that adds 2 integers. updated due to the pycodestyle

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,22 +5,22 @@
 
 
 def add_integer(a, b=98):
-    # a must be an integer ELSE raise TypeError
+    '''a must be an integer ELSE raise TypeError'''
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError('a must be an integer')
 
-    # b must be an integer ELSE raise TypeError
+    '''b must be an integer ELSE raise TypeError'''
     if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError('b must be an integer')
 
-    # typecast a to int from float
+    '''typecast a to int from float'''
     if isinstance(a, float):
         a = int(a)
 
-    # typecast b to int from float
-    if isinstance(b,float):
+    '''typecast b to int from float'''
+    if isinstance(b, float):
         b = int(b)
 
-    # Return the sum of a and b = (int)
+    '''Return the sum of a and b = (int)'''
     sum = a + b
     return sum
