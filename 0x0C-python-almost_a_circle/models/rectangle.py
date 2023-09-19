3ddbbcf937d12c665ba95de15db59360af984650
lawal tajudeen ogunsola <lwltjdn@gmail.com>
2023-09-19 12:57:34 +0100

    Rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -137,7 +137,11 @@ class Rectangle(Base):
                 it returns:
                     [Rectangle] (<id>) <x>/<y> - <width>/<height>
         '''
-        return '[Rectangle] ({}) {}/{} - {}/{}'.format(self.id, self.x, self.y, self.width, self.height)
+        return '[Rectangle] ({}) {}/{} - {}/{}'.format(self.id,
+                                                       self.x,
+                                                       self.y,
+                                                       self.width,
+                                                       self.height)
 
     def update(self, *args, **kwargs):
         '''
