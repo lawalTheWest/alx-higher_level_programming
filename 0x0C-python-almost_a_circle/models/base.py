e89b4c1395fbfe913737d7886669dafd68870d4f
lawal tajudeen ogunsola <lwltjdn@gmail.com>
2023-09-22 11:02:59 +0100

    updaated the base case class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -45,5 +45,4 @@ class Base:
         '''
         if list_dictionaries is None or list_dictionaries == []:
             return '[]'
-        return json.dumps(list_dictionaries
-
+        return json.dumps(list_dictionaries)
