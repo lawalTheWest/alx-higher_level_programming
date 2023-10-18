aa362996cdccef9ea80d1595757ab62db00357bc
Lawal Tajudeen Ogunsola <lwltjdn@gmail.com>
2023-10-18 03:53:49 +0100

    a script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,7 @@
+-- This script lists all records of the table second_table having
+-- a name value in my MySQL server.
+-- Records are ordered by descending score.
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `name` != ""
+ORDER BY `score` DESC
