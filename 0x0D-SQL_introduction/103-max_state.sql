b069ce950c5776e0de8741b89b1a075826838ca1
Lawal Tajudeen Ogunsola <lwltjdn@gmail.com>
2023-10-18 04:20:29 +0100

    a script that displays the max temperature of each state (ordered by State name).

---
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -0,0 +1,6 @@
+-- This script displays the max temperature of each state,
+-- ordered by state name.
+SELECT `state`, MAX(`value`) AS `max_temp`
+FROM `temperatures`
+GROUP BY `state`
+ORDER BY `state`;
