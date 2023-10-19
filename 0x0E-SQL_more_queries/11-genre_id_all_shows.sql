8910a7e2a32116abce918d1bbf6fb6a4c8d542e8
Lawal Tajudeen Ogunsola <lwltjdn@gmail.com>
2023-10-19 03:31:52 +0100

    a script that lists all shows contained in the database hbtn_0d_tvshows.
    Each record should display: tv_shows.title - tv_show_genres.genre_id
    Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
    If a show doesn’t have a genre, display NULL
    You can use only one SELECT statement
    The database name will be passed as an argument of the mysql command

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,10 @@
+-- This script lists all the shows contained in
+-- the database hbtn_0d_tvshows.
+-- It displays NULL for shows without genres.
+-- Records are ordered by ascending tv_shows.title
+-- and tv_show_genres.genre_id.
+SELECT s.`title`, g.`genre_id`
+	FROM `tv_shows` AS s
+		LEFT JOIN `tv_show_genres` AS g
+		ON s.`id` = g.`show_id`
+	ORDER BY s.`title`, g.`genre_id`;
