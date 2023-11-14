44a07b63a80ca79fb13e971bf0dc50dbead3c6f4
Lawal Tajudeen Ogunsola <lwltjdn@gmail.com>
2023-11-14 15:17:35 +0100

    to integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -10,5 +10,5 @@ const num = Math.floor(Number(process.argv[2]));
 if (isNaN(num)) {
   console.log('Not a number');
 } /* end if */ else {
-  console.log('My number:' + num);
+  console.log('My number: ' + num);
 } /* End else */
