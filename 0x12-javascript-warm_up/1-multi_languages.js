281526b9c26fe6a7be8179bf365a08b55b3a065c
Lawal Tajudeen Ogunsola <lwltjdn@gmail.com>
2023-11-14 03:57:55 +0100

    the script prints multilines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+// Script that prints to the console
+const firstLine = 'C is fun';
+const secondLine = 'Python is cool';
+const thirdLine = 'JavaScript is amazing';
+
+console.log(firstLine + '\n', secondLine + '\n', thirdLine);
