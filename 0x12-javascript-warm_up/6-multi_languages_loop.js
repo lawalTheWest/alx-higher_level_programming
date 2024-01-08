5c4d86a7f89903dd779538819c024bde08aa6924
Lawal Tajudeen O. (The West) <lwltjdn@gmail.com>
2024-01-08 09:07:14 +0100

    Delete 0x12-javascript-warm_up directory
    removed

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-
-const args = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (const arg in args) {
-  console.log(args[arg]);
-} /* End for loop */
--- 0x12-javascript-warm_up/9-add.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-// function that adds two integers
-function add (a, b) {
-  return a + b;
-} /* End function */
-console.log(add(Number(process.argv[2]), Number(process.argv[3])));
