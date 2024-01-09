73cd659f26e8b4380919b1db662a8e70c829ad1d
Tajudeen <lwltjdn@gmail.com>
2024-01-09 03:41:10 +0100

    js arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+// prints based on arguments given
+const { argv } = require('process');
+
+// getting the lenght of arguments
+const argvLength = argv.length - 2;
+
+// validate parameters
+if (argvLength <= 0) {
+  console.log('No argument');
+} /* end if */ else if (argvLength === 1) {
+  console.log('Argument found');
+} /* end else if */ else {
+  console.log('Arguments found');
+} /* end else */
