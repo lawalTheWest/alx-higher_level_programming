be7fe30270874392e7d826ea7b3cc662b5a1b4e0
Tajudeen <lwltjdn@gmail.com>
2024-01-09 11:42:58 +0100

    the rectangle class

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/* Defines the Rectangle class */
+
+class Rectangle {
+  /* defines rectangle */
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height =h;
+    } /* end if */
+  } /* ends constructor */
+} /* End class */
+module.exports = Rectangle;
