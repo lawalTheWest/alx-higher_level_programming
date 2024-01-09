e2234bdf06b8828d1c671470b188dc7590d7e03a
Tajudeen <lwltjdn@gmail.com>
2024-01-09 15:05:06 +0100

    resolved a semistandard issue

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -6,7 +6,7 @@ class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
-      this.height =h;
+      this.height = h;
     } /* end if */
   } /* ends constructor */
 } /* End class */
