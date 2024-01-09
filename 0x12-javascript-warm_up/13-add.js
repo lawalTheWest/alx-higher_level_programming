335bcc2d683ac5794ac19f7dd060837b41d6f667
Tajudeen <lwltjdn@gmail.com>
2024-01-09 03:50:33 +0100

    adding two integers together

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+/*
+ * a function that returns the addition of 2 integers.
+ *  The function must be visible from outside
+ *  The name of the function must be add
+ *  You are not allowed to use var
+ */
+exports.add = (a, b) => a + b;
