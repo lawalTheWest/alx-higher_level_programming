89b15dd80c0f61f94f7c9bb72f81696d964bb500
Tajudeen <lwltjdn@gmail.com>
2024-01-09 15:17:19 +0100

    Resolved the issue of semistandard

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -3,7 +3,7 @@
 module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
-  } /*End the constructor */
+  } /* End the constructor */
 
   print () {
     for (let i = 0; i < this.height; i++) {
