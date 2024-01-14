02b33cc56691ef4694f1e3e637a062e1560d1fe4
Tajudeen <lwltjdn@gmail.com>
2024-01-14 16:16:31 +0100

    edited to fix errors

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -27,4 +27,5 @@ if __name__ == "__main__":
     for instance in session.query(State).filter(State.name.contains('a')):
         print('{0}: {1}'.format(
                                 instance.id,
-                                instance.name)) # ends for
+                                instance.name))
+        # ends for
