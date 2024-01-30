b71bf6da3d0bfa10ff9d17a3f23145356006ff45
Tajudeen <lwltjdn@gmail.com>
2024-01-30 03:17:38 +0100

    takes and requests url

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+    This script takes in a URL and sends a request to the URL
+    and displays the value of the variable found in the header of the response
+'''
+import urllib.request
+import sys
+
+if __name__ == '__main__':
+    my_request = urllib.request.Request(argv[1])
+    with urllib.request.urlopen(my_request) as response:
+        value = response.getheader('X-Request-Id')
+        print(value)
