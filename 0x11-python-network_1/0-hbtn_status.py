6c1b978f582dbec55c6dfc170684dc08cd0d8790
Tajudeen <lwltjdn@gmail.com>
2024-02-01 11:20:53 +0100

    fetches the url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 '''
     The python script that fetches "https://alx-intranet.hbtn.io/status"
+
+    Usage: ./0-hbtn_status.py | cat -e
 '''
 import urllib.request
 with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
     html = response.read()
+    print('Body response:')
+    print('content:')
