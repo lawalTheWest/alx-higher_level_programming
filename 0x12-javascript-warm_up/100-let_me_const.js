3509af513f9b7b738b5fa1276029b6b3ae834240
LawalTheWest <lwltjdn@gmail.com>
2024-02-25 12:17:05 +0100

    removed the warm up directory

--- 0x12-javascript-warm_up/100-let_me_const.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/node
-
-// Modifies the value of a variable
-myVar = 333;
--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-
-/*  increments and calls a function. */
-exports.addMeMaybe = function (number, theFunction) {
-  theFunction(++number);
-};
