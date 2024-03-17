ef3bacef3098680cf3f527e66bf80c692c591ac9
Tajudeen <lwltjdn@gmail.com>
2024-03-17 11:13:00 +0100

    closed the db after usage

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -22,23 +22,33 @@ if __name__ == '__main__':
             B. script should connect to a MySQL server
                 running on localhost at port 3306
     '''
-    my_db = MySQLdb.connect(host="localhost",
+    db = MySQLdb.connect(host="localhost",
                             user=argv[1],
                             port="3306",
                             password=argv[2],
-                            my_db=argv[3])
+                            db=argv[3])
 
     '''
         Creating a cursor object for my_db:
             more details in the documentation file
             README.md
     '''
-    my_db_cursor_obj = my_db.cursor()
-    my_db_cursor_obj.execute("SELECT * FROM states")
-    rows = my_db_cursor_obj.fetchall()
+    db_cursor_obj = db.cursor()
+    db_cursor_obj.execute("SELECT * FROM states")
+    rows = db_cursor_obj.fetchall()
 
     '''
         Iterate through the db and print all the states
     '''
     for row in rows:
         print(row)
+
+    '''
+        Close the cursor and database connection
+    '''
+    db_cursor_obj.close()
+    db.close()
+
+    except MySQLdb.Error as e:
+        print("MySQL Error:", e)
+        exit(1)
