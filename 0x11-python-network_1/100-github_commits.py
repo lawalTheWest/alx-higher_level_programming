2a8112eb86d7756092d72d272e6a565e512f1545
LawalTheWest <lwltjdn@gmail.com>
2024-04-01 14:55:35 +0100

    pep8 issues. Usage: ./100-github_commits.py repo-name author-name

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     import requests
     from sys import argv
     rqst = requests.get('https://api.github.com/repos/{}/{}/commits'
-                     .format(argv[2], argv[1]))
+                        .format(argv[2], argv[1]))
     commits = rqst.json()
     for commit in commits[:10]:
         print(commit.get('sha'), end=': ')
